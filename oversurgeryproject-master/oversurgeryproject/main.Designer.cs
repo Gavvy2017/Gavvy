e26fc50340f3286fc69f1705e7c1798a3efa654f
GAVVY <gavvy.singh@student.anglia.ac.uk>
2017-12-14 19:29:25 +0000

    Appointment Design
    This week I done appointment design for my week 7

--- oversurgeryproject-master/oversurgeryproject/main.Designer.cs
+++ oversurgeryproject-master/oversurgeryproject/main.Designer.cs
@@ -43,7 +43,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(279, 246);
+            this.button1.Location = new System.Drawing.Point(262, 255);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(115, 79);
