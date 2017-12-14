29b2348bb8106c74508a35c22b8b3164c6643f0f
GAVVY <gavvy.singh@student.anglia.ac.uk>
2017-12-14 21:08:24 +0000

    Prescription Design Form and Implemented
    I done a design for the prescription including the code

--- oversurgeryproject-master/oversurgeryproject/Prescription.Designer.cs
+++ oversurgeryproject-master/oversurgeryproject/Prescription.Designer.cs
@@ -28,19 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Prescription));
             this.button7 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.oversugeryDBDataSet = new oversurgeryproject.OversugeryDBDataSet();
+            this.oversugeryDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Medication = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oversugeryDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oversugeryDBDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // button7
             // 
             this.button7.BackColor = System.Drawing.Color.ForestGreen;
-            this.button7.Location = new System.Drawing.Point(634, 12);
+            this.button7.Location = new System.Drawing.Point(822, 20);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(112, 36);
+            this.button7.Size = new System.Drawing.Size(84, 29);
             this.button7.TabIndex = 20;
             this.button7.Text = "Back";
             this.button7.UseVisualStyleBackColor = false;
@@ -49,9 +58,10 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.ForestGreen;
-            this.button1.Location = new System.Drawing.Point(765, 12);
+            this.button1.Location = new System.Drawing.Point(920, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 36);
+            this.button1.Size = new System.Drawing.Size(84, 29);
             this.button1.TabIndex = 21;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = false;
@@ -60,24 +70,58 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(634, 54);
+            this.pictureBox1.Location = new System.Drawing.Point(822, 54);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(243, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(182, 41);
             this.pictureBox1.TabIndex = 22;
             this.pictureBox1.TabStop = false;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Medication});
+            this.dataGridView1.DataSource = this.oversugeryDBDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 232);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(951, 187);
+            this.dataGridView1.TabIndex = 23;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // oversugeryDBDataSet
+            // 
+            this.oversugeryDBDataSet.DataSetName = "OversugeryDBDataSet";
+            this.oversugeryDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // oversugeryDBDataSetBindingSource
+            // 
+            this.oversugeryDBDataSetBindingSource.DataSource = this.oversugeryDBDataSet;
+            this.oversugeryDBDataSetBindingSource.Position = 0;
+            // 
+            // Medication
+            // 
+            this.Medication.HeaderText = "Column1";
+            this.Medication.Name = "Medication";
+            // 
             // Prescription
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Red;
-            this.ClientSize = new System.Drawing.Size(889, 507);
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(1045, 479);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button7);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Prescription";
             this.Text = "Prescription";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oversugeryDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oversugeryDBDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,5 +131,9 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource oversugeryDBDataSetBindingSource;
+        private OversugeryDBDataSet oversugeryDBDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Medication;
     }
 }
--- oversurgeryproject-master/oversurgeryproject/Prescription.cs
+++ oversurgeryproject-master/oversurgeryproject/Prescription.cs
@@ -17,18 +17,55 @@ namespace oversurgeryproject
             InitializeComponent();
         }
 
-        private void button7_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            patient mm = new patient();
-            mm.Show();
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             this.Hide();
+            main mm = new main();
+            mm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //This code shows Gurvinder adding the  button so it will link up with the forms and interact
+            this.Hide();
             Login mm = new Login();
             mm.Show();
+        }
+
+        private void Staff_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'oversugeryDBDataSet3.staff' table. You can move, or remove it, as needed.
+            this.staffTableAdapter.Fill(this.oversugeryDBDataSet3.staff);
+
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Chad\Documents\GitHub\oversurgeryproject\oversurgeryproject\OversugeryDB.mdf;Integrated Security=True;Connect Timeout=30");
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //SqlDataAdapter sda = new SqlDataAdapter("select count(*) from staff where id ='" + comboBox1.Text + "'", conn);
+            //DataTable dt = new DataTable();
+            //sda.Fill(dt);
+            //dataGridView1.DataSource = dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string local = textBox1.Text;
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Chad\Documents\GitHub\oversurgeryproject\oversurgeryproject\OversugeryDB.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter da = new SqlDataAdapter("select * from staff where id = '" + local + "'", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
